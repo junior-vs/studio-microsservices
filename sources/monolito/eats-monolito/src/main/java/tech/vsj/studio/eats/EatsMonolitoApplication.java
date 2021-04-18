dc46cf047ef127a1155a7bcb23ecf3968e9c1616
Valdir Junior <junior_vs@outlook.com>
2021-04-18 17:03:35 -0300

    start no projeto monolito

---
+++ sources/monolito/eats-monolito/src/main/java/tech/vsj/studio/eats/EatsMonolitoApplication.java
@@ -0,0 +1,13 @@
+package tech.vsj.studio.eats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsMonolitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatsMonolitoApplication.class, args);
+	}
+
+}
---
+++ sources/monolito/eats-monolito/src/test/java/tech/vsj/studio/eats/EatsMonolitoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.vsj.studio.eats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsMonolitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
